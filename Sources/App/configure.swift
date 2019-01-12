aece62b32dcbb9091d5cdecce7c01838d6bb7350
Robin Senior <robin.senior@shopify.com>
2019-01-11 21:45:55 -0500

    first commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,6 +15,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+	middlewares.use(SessionsMiddleware.self) // Federated authentication
     services.register(middlewares)
 
     // Configure a SQLite database
