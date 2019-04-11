06c836b1e695dba197989934279e866c1711be9e
Robin Senior <robin.senior@shopify.com>
2019-04-11 16:13:39 -0400

    update packages

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "shopify-mock-marketing",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
         // Federated login
-        .package(url: "https://github.com/davidmuzi/Imperial.git", from: "0.4.0"),
+        .package(url: "https://github.com/vapor-community/Imperial.git", .exact("0.10.0")),
         .package(url: "https://github.com/davidmuzi/shopify-api.git", from: "0.0.1"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc")
